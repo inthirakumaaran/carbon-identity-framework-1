8fd532203ce15ef2e793aff01db67cab5116ce38
Dinali Dabarera <gdrdabarera@gmail.com>
2020-01-03 09:03:17 +0530

    Combining .class files to a jar inside the webapps (#903)
    * login page
    
    * Fix  username recovery flow
    
    * fix password revery and username recovery flow
    
    * Changed the module structure remove .class files in web apps
    
    * Changed package name to previous package name
    
    * reverted web app changes
    
    * refactored authentication endpoint java class packages
    
    * updated import packages

---
+++ components/identity-mgt/org.wso2.carbon.identity.mgt.endpoint.util/ReadMe.md
@@ -0,0 +1,3 @@
+# This package decouples java source files from the recoveryendpoint webapp, by packing all the java source files to a separate bundle.
+
+All the java classes that bundled in this component goes to the <accountrecoveryendpoint>/WEB-INF/lib folder as a org.wso2.carbon.identity.mgt.endpoint.util-5.14.97.jar.
---
+++ components/identity-mgt/org.wso2.carbon.identity.mgt.endpoint.util/src/main/java/org/wso2/carbon/identity/mgt/endpoint/serviceclient/beans/Claim.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.mgt.endpoint.serviceclient.beans;
+
+public class Claim {
+    private String claimUri;
+    private boolean readOnly;
+    private boolean checkedAttribute;
+    private String displayTag;
+    private String description;
+    private boolean supportedByDefault;
+    private boolean required;
+    private String regEx;
+    private String dialectURI;
+    private String value;
+    private int displayOrder;
+
+
+    public String getClaimUri() {
+        return claimUri;
+    }
+
+    public void setClaimUri(String claimUri) {
+        this.claimUri = claimUri;
+    }
+
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    public void setReadOnly(boolean readOnly) {
+        this.readOnly = readOnly;
+    }
+
+    public boolean isCheckedAttribute() {
+        return checkedAttribute;
+    }
+
+    public void setCheckedAttribute(boolean checkedAttribute) {
+        this.checkedAttribute = checkedAttribute;
+    }
+
+    public String getDisplayTag() {
+        return displayTag;
+    }
+
+    public void setDisplayTag(String displayTag) {
+        this.displayTag = displayTag;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isSupportedByDefault() {
+        return supportedByDefault;
+    }
+
+    public void setSupportedByDefault(boolean supportedByDefault) {
+        this.supportedByDefault = supportedByDefault;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    public String getRegEx() {
+        return regEx;
+    }
+
+    public void setRegEx(String regEx) {
+        this.regEx = regEx;
+    }
+
+    public String getDialectURI() {
+        return dialectURI;
+    }
+
+    public void setDialectURI(String dialectURI) {
+        this.dialectURI = dialectURI;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getDisplayOrder() {
+        return displayOrder;
+    }
+
+    public void setDisplayOrder(int displayOrder) {
+        this.displayOrder = displayOrder;
+    }
+}
