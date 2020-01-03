9afdc86a52876d055e74ca0f4cd47ea57f6d1eff
Dinali Dabarera <gdrdabarera@gmail.com>
2020-01-03 09:42:57 +0530

    Revert "Combining .class files to a jar inside the webapps (#903)" (#907)
    This reverts commit 8fd532203ce15ef2e793aff01db67cab5116ce38.

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.endpoint.util/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/util/TenantMgtAdminServiceClient.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.endpoint.util/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/util/TenantMgtAdminServiceClient.java
@@ -21,6 +21,7 @@ package org.wso2.carbon.identity.application.authentication.endpoint.util;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.application.authentication.endpoint.util.AuthenticationException;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
